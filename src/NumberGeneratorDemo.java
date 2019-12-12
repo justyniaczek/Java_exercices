b3628a54a3642d3637479551f21267497328d0c7
justyniaczek <44835858+justyniaczek@users.noreply.github.com>
2019-12-12 21:48:00 +0100

    Add files via upload

---
+++ src/NumberGeneratorDemo.java
@@ -0,0 +1,7 @@
+public class NumberGeneratorDemo {
+    public static void main(String[] args) {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        numberGenerator.generateNumbers();
+        System.out.println(numberGenerator.getNumberGenerator());
+    }
+}
